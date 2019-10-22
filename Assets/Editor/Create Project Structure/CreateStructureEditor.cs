d511c1502032ed0faf03e27a925b6ddb47fbc0d6
Mohamedaraby <mohamedaraby1296@gmail.com>
2019-10-22 16:14:17 +0200

    initial

---
+++ Assets/Editor/Create Project Structure/CreateStructureEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace MyTools
+{
+    public class CreateStructureEditor : Editor
+    {
+        [MenuItem("MyTools/Projects/Project Setup Tool")]
+        public static void InitProjectSetupTool()
+        {
+            CreateStructureWindow.InitWindow();
+        }
+    }
+}
---
+++ Assets/Editor/Create Project Structure/CreateStructureWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace MyTools
+{
+    public class CreateStructureWindow : EditorWindow
+    {
+        static CreateStructureWindow mainWindow;
+
+
+
+        public static void InitWindow()
+        {
+            mainWindow = EditorWindow.GetWindow<CreateStructureWindow>("Create Structure Window");
+            mainWindow.Show();
+        }
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Create Structure Window");
+        }
+    }
+}
